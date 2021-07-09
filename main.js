1acaedb0056425200104bff3dc7e6c1d75fb62c4
pragnaybhati <82753803+pragnaybhati@users.noreply.github.com>
2021-07-09 20:53:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+noseX = "";
+noseY = "";
+leftWristX = "";
+rightWristX = "";
+difference = "";
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(370, 370);
-video.position(50, 150)
+    video.position(50, 150)
     canvas = createCanvas(370, 370);
     canvas.position(650, 150);
 
@@ -13,12 +19,21 @@ function modal_loaded() {
     console.log("Pose is Initialized");
 }
 
-function draw() {
-    background("lightgrey");
-}
-
 function got_poses(results) {
     if (results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("Nose X = " + noseX + " Nose Y = " + noseY);
+        console.log("Left Wrist X = " + leftWristX + " Right Wrist X = " + rightWristX);
+        difference = floor(leftWristX - rightWristX);
     }
+}
+function draw() {
+    background("grey");
+    textSize(difference);
+    fill("white");
+    text(" Pragnay " , noseX , noseY);
 }
